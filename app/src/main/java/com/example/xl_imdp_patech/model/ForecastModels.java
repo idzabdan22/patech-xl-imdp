69db7115cc862b7c098d45f784cd8e0d901e81bd
Auliyaf <auliyafirdaus03@gmail.com>
2021-09-27 18:02:14 +0700

    add tflite

--- app/src/main/java/com/example/xl_imdp_patech/model/ForecastModels.java
+++ app/src/main/java/com/example/xl_imdp_patech/model/ForecastModels.java
@@ -1,6 +1,15 @@
 package com.example.xl_imdp_patech.model;
 
+import android.os.Bundle;
+
+import org.tensorflow.lite.Interpreter;
+
 public class ForecastModels {
+    Interpreter tflite;
+  //  @Override
+  //  protected void onCreate (Bundle SavedInstanceState){
+  //      super.onCreate(SavedInstanceState);
+  //      setContentView(R.Layout.activity_main);
 
 
 
